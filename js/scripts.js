d207911d430e74b97b59e31d2d075249a63f37e4
Ryan Dikeos and Tracy Jo Reith <tracy@>
2019-01-16 11:35:05 -0800

    add forms for input, text for output

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,15 @@ function divide(number1, number2)
 // var number2 = parseInt(prompt("Enter another number:"));
 // var result = add(number1, number2);
 // alert(result);
+
+// Everything below this line is user interface (or front-end) logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
