a83d9f3cfd055a7eb366c7cacbd6e8551d2a69cc
Ryan Dikeos and Tracy Jo Reith <tracy@>
2019-01-16 11:17:17 -0800

    Setup project for calculator with bootstrap and jquery, comment out prompt and alert javascript

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ function divide(number1, number2)
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+// var result = add(number1, number2);
+// alert(result);
